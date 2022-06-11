58c292d176034ddfaa651bcd7207a2b896d89d98
Natanael Silva <natanael96@live.com>
2022-06-11 23:52:51 +0000

    initial commit

---
+++ src/test/java/br/com/parkingcontrolapi/ParkingControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.parkingcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
