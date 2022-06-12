a9c1baed836dd80c70279c29b4e55e19557ea97c
Natanael Silva <natanael96@live.com>
2022-06-12 01:22:13 +0000

    feat: create parking spot dto

---
+++ src/main/java/br/com/parkingcontrolapi/dtos/ParkingSpotDto.java
@@ -0,0 +1,37 @@
+package br.com.parkingcontrolapi.dtos;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String brandCar;
+
+    @NotBlank
+    private String modelCar;
+
+    @NotBlank
+    private String colorCar;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String apartment;
+
+    @NotBlank
+    private String block;
+}
