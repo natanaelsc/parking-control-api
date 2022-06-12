7bdae655fc7bbab2a1b59ef1ac78e4f5ff1e1c99
Natanael Silva <natanael96@live.com>
2022-06-12 01:26:22 +0000

    feat: create parking spot repository

--- src/main/java/br/com/parkingcontrolapi/repositories/ParkingSpotRepository.java
+++ src/main/java/br/com/parkingcontrolapi/repositories/ParkingSpotRepository.java
@@ -1,5 +1,16 @@
 package br.com.parkingcontrolapi.repositories;
 
-public class ParkingSpotRepository {
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.parkingcontrolapi.models.ParkingSpotModel;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
 }
